13075ee0e36fb4436d550693aa28c9476fc09e64
ManavRakholiya <manavrakholiya1532006@gmail.com>
2025-07-23 14:29:24 +0530

    23/07/25 update

--- Matchmaking.java
+++ Matchmaking.java
@@ -4,41 +4,43 @@ public class Matchmaking
 {
     static PriorityQueue<UserMatch> matchMadeUsingHobby() {
         PriorityQueue<UserMatch> queue = new PriorityQueue<>();
-        try {
-            int myId = CurrentUser.data.getId();
-            char myGender = CurrentUser.data.getGender();
-            ArrayList<Hobby> myHobbiesList = DatabaseIO.getHobbiesFromUID(myId);
-            HashSet<Integer> myHobbyIds = new HashSet<>();
-            for (Hobby h : myHobbiesList) {
-                myHobbyIds.add(h.hobbyId);
-            }
 
-            // Get all users
-            ArrayList<User> users = DatabaseIO.getUsers();
-            for (User user : users) {
-                // Skip self and same gender or inactive
-                if (user.getId() == myId) continue;
-                if (!user.isActive || user.getGender() == myGender) continue;
-
-                // Get hobbies for this user
-                ArrayList<Hobby> theirHobbies = DatabaseIO.getHobbiesFromUID(user.getId());
-                HashSet<Integer> theirHobbyIds = new HashSet<>();
-                for (Hobby h : theirHobbies) {
-                    theirHobbyIds.add(h.hobbyId);
+            try {
+                int myId = CurrentUser.data.getId();
+                char myGender = CurrentUser.data.getGender();
+                ArrayList<Hobby> myHobbiesList = DatabaseIO.getHobbiesFromUID(myId);
+                HashSet<Integer> myHobbyIds = new HashSet<>();
+                for (Hobby h : myHobbiesList) {
+                    myHobbyIds.add(h.hobbyId);
                 }
 
-                // Calculate number of shared hobbies
-                HashSet<Integer> common = new HashSet<>(myHobbyIds);
-                common.retainAll(theirHobbyIds);
-                int shared = common.size();
+                // Get all users
+                ArrayList<User> users = DatabaseIO.getUsers();
+                for (User user : users) {
+                    // Skip self and same gender or inactive
+                    if (user.getId() == myId) continue;
+                    if (!user.isActive || user.getGender() == myGender) continue;
 
-                if (shared > 0) {
-                    queue.add(new UserMatch(user, shared));
+                    // Get hobbies for this user
+                    ArrayList<Hobby> theirHobbies = DatabaseIO.getHobbiesFromUID(user.getId());
+                    HashSet<Integer> theirHobbyIds = new HashSet<>();
+                    for (Hobby h : theirHobbies) {
+                        theirHobbyIds.add(h.hobbyId);
+                    }
+
+                    // Calculate number of shared hobbies
+                    HashSet<Integer> common = new HashSet<>(myHobbyIds);
+                    common.retainAll(theirHobbyIds);
+                    int shared = common.size();
+
+                    if (shared > 0) {
+                        queue.add(new UserMatch(user, shared));
+                    }
                 }
+            } catch (Exception e) {
+                Log.E("Error matching by hobby: " + e.getMessage());
             }
-        } catch (Exception e) {
-            Log.E("Error matching by hobby: " + e.getMessage());
-        }
+
         return queue;
     }
 
